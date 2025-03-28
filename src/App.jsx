246c46f3c32d96f725dea6296db970680302b204
Oslynx <malakayalvg@gmail.com>
2025-03-28 10:57:28 +0100

    Login / Sign Up Form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LoginSignup from "./components/LoginSignup.jsx";
+
+function App() {
+
+  return (
+    <>
+        <LoginSignup />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/LoginSignup.jsx
@@ -0,0 +1,34 @@
+import "../css/LoginSignup.css"
+import {useState} from "react";
+
+const LoginSignup = () => {
+
+    const [ action, setAction ] = useState("Sign Up")
+
+    return(
+        <>
+            <div className="main-container px-5">
+                <div className="header">
+                    <h1>{action}</h1>
+                    <div className="underline"></div>
+                </div>
+                <div className="inputs">
+                    <div className="input">
+                        <i className="bi bi-person mx-3 fs-4"></i>
+                        <input type="text" placeholder="Username"/>
+                    </div>
+                    <div className="input">
+                        <i className="bi bi-key mx-3 fs-4"></i>
+                        <input type="password" placeholder="Password"/>
+                    </div>
+                </div>
+                <div className="submit-container">
+                    <div className={action === "Login"?"submit grey":"submit"} onClick={()=>setAction("Sign Up")}>Sign up</div>
+                    <div className={action === "Sign Up"?"submit grey":"submit"} onClick={() => setAction("Login")}>Login</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LoginSignup;
